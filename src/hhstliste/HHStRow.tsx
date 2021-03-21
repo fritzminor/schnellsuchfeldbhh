f40e19c8c68dcd05092de72f67e6ff213a5830e1
fritzminor <nabf@arcor.de>
2021-03-21 08:43:34 +0100

    Feature: Rounded bottom corners for epl subtotals.

--- src/hhstliste/HHStRow.tsx
+++ src/hhstliste/HHStRow.tsx
@@ -1,29 +1,48 @@
 import * as React from "react";
 import { formatBetrag } from "../store/AppState";
-import {  HHStOrBlock } from "../store/HHStType";
+import { HHStOrBlock } from "../store/HHStType";
 
 type HHStRowProps = {
   hhst: HHStOrBlock;
   heading?: boolean;
 };
 
-export function HHStRow({ hhst, heading }: HHStRowProps): JSX.Element {
-  
-  const className = heading || ( hhst.type=="block" )
-    ? "container hhstRow is-size-6 has-background-primary"
-    : "container hhstRow is-size-7";
+export function HHStRow({
+  hhst,
+  heading
+}: HHStRowProps): JSX.Element {
+  let className = "container hhstRow";
+  if (heading || hhst.type == "block") {
+    className += " is-size-6";
+    if (heading || !hhst.epl)
+      className += " has-background-info has-text-light";
+    else {
+      className += " has-background-primary";
+    }
+    if (
+      hhst.type === "block" &&
+      !hhst.blockstart &&
+      hhst.expense
+    )
+      className += " blockEnd";
+  } else className += " is-size-7";
+
   return (
     <div className={className}>
       <div className="hhstNr">
         <span>{hhst.epl}</span>
         <span> {hhst.kap}</span>
-        <span>{hhst.gruppe?` / ${hhst.gruppe}`:""}</span>
+        <span>
+          {hhst.gruppe ? ` / ${hhst.gruppe}` : ""}
+        </span>
         <span> {hhst.suffix} </span>
         <span className="fkz">{hhst.fkz}</span>
       </div>
       <div className="zweck">{hhst.zweck}</div>
       <div className="soll1">
-        {heading ? hhst.sollJahr1 : formatBetrag(hhst.sollJahr1)}
+        {heading
+          ? hhst.sollJahr1
+          : formatBetrag(hhst.sollJahr1)}
       </div>
     </div>
   );
