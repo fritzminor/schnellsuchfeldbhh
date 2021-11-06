cffdfe712088778f122b1a9a1b555b22dc59d36d
fritzminor <nabf@arcor.de>
2021-11-06 09:46:17 +0100

    Refactor: moved compareHHSt to separate file.

--- src/import/importHOLTitGr.ts
+++ src/import/importHOLTitGr.ts
@@ -7,6 +7,7 @@ import {
   openFile,
   TabularContent
 } from "./analyzePDF";
+import { compareHHSt } from "../store/compareHHSt";
 
 /** This method expects a HOL-PDF of the Titelgruppen in DIN A4 landscape.
  * Before calling this method, the base-hhsts has to be loaded from a
@@ -284,40 +285,4 @@ export function analyzeTabContent(
   };
 }
 
-function compareHHSt(a: HHSt, b: HHSt): number {
-  const cmpEplKap = (a.epl + a.kap).localeCompare(
-    b.epl + b.kap
-  );
-  if (cmpEplKap) return cmpEplKap;
 
-  if (a.expense) {
-    if (!b.expense) return 1; // a comes after b
-  } else {
-    // a is revenue
-    if (b.expense) return -1; // a comes before b
-  }
-
-  if (a.tgKey) {
-    if (b.tgKey) {
-      const cmpTgKey = a.tgKey.localeCompare(b.tgKey);
-      if (cmpTgKey) return cmpTgKey;
-    } else {
-      // a has tgKey, b has not.
-      return 1; // a comes after b
-    }
-  } else {
-    // a.tgKey is undefined
-    if (b.tgKey) {
-      // a has got no tgKey, b has got one.
-      return -1; // b comes after a
-    }
-  }
-
-  const cmpTitelNr = (a.gruppe + a.suffix).localeCompare(
-    b.gruppe + b.suffix
-  );
-  if (cmpTitelNr === 0) {
-    console.log("Equal:", a, b);
-  }
-  return cmpTitelNr;
-}
---
+++ src/store/compareHHSt.ts
@@ -0,0 +1,59 @@
+import { HHSt } from "./HHStType";
+
+/**
+ * compares two hhsts.
+ * the sorted hhsts are ordered by
+ *  - epl
+ *  - kap
+ *  - revenues before expenses
+ *  - non-tgKey before tgKey
+ *  - gruppe
+ *  - suffix
+ * 
+ * Otherwise the hhst is considered equal, i.e. even if zweck or 
+ * sollJahr1 differ, the function returns 0
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+export function compareHHSt(a: HHSt, b: HHSt): number {
+  const cmpEplKap = (a.epl + a.kap).localeCompare(
+    b.epl + b.kap
+  );
+  if (cmpEplKap)
+    return cmpEplKap;
+
+  if (a.expense) {
+    if (!b.expense)
+      return 1; // a comes after b
+  } else {
+    // a is revenue
+    if (b.expense)
+      return -1; // a comes before b
+  }
+
+  if (a.tgKey) {
+    if (b.tgKey) {
+      const cmpTgKey = a.tgKey.localeCompare(b.tgKey);
+      if (cmpTgKey)
+        return cmpTgKey;
+    } else {
+      // a has tgKey, b has not.
+      return 1; // a comes after b
+    }
+  } else {
+    // a.tgKey is undefined
+    if (b.tgKey) {
+      // a has got no tgKey, b has got one.
+      return -1; // b comes after a
+    }
+  }
+
+  const cmpTitelNr = (a.gruppe + a.suffix).localeCompare(
+    b.gruppe + b.suffix
+  );
+  if (cmpTitelNr === 0) {
+    console.log("Equal:", a, b);
+  }
+  return cmpTitelNr;
+}
